bd4fe3bbfa80cbbf4425241b5458b4bce6c804e4
Ali Sait Teke <alisaitteke@gmail.com>
2024-09-10 23:57:02 +0100

    .gitignore

---
+++ dist/components/TypebotBubbleComponent.vue.d.ts
@@ -0,0 +1,13 @@
+declare const _default: import("vue").DefineComponent<import("vue").ExtractPropTypes<{
+    config: {
+        type: ObjectConstructor;
+        required: true;
+    };
+}>, () => void, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{
+    config: {
+        type: ObjectConstructor;
+        required: true;
+    };
+}>> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
+export default _default;
+//# sourceMappingURL=TypebotBubbleComponent.vue?vue&type=script&setup=true&lang.d.ts.map
